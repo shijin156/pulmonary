aa6424effd622e0101dee6e9eec6804bca1b0ec7
shijin156 <39981227+shijin156@users.noreply.github.com>
2023-06-15 21:30:32 +0530

    New changes

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -17,6 +17,7 @@ class CategoriesController extends Controller
     {
         $categories = Category::orderBy('name', 'asc')->get();
         return view('categories.all', compact('categories'));
+        
     }
 
     /**
